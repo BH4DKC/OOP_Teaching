1fd6015b4c3a7c20784476dbe717332ea5dd435d
BH4DKC <zhiqimao@qq.com>
2026-05-31 09:54:07 -0500

    Adjusted output overwrite

--- UNO_Game/card.py
+++ UNO_Game/card.py
@@ -4,4 +4,9 @@ class Card:
         self.value = value
 
     def __str__(self):
-        return f"{self.color} {self.value}"
+        return (f"----------"
+                f"color: {self.color}"
+                f"value: {self.value}"
+                f"---------")
+
+
